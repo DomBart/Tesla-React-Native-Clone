8b82a5b7f70b54bb24386c9f6ef8b225e1bb0031
DomBart <domantas.bartusevicius@gmail.com>
2021-02-09 00:25:06 +0200

    List element scroll snap, header component

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, Dimensions} from 'react-native';
 import CarItem from '../CarItem';
 import styles from './styles';
 import cars from './cars';
@@ -10,7 +10,11 @@ const CarsList = (props) => {
         <View style={styles.container}>
             <FlatList
               data={cars}
-              renderItem={({item}) => <CarItem car={item}/>} 
+              renderItem={({item}) => <CarItem car={item}/>}
+              snapToAlignment={'start'}
+              decelerationRate={'fast'}
+              snapToInterval={Dimensions.get('window').height}
+              showsVerticalScrollIndicator={false}
             />
         </View>
     );
